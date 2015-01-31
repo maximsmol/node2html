c5cb3bbd7af0db1baa36f766eea26a289ab9ec77
maximsmol <maximsmol@aristotle.local>
2015-01-31 09:42:26 +0300

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,29 @@
+'use strict';
+
+//
+// Standard libraries
+var assert = require('assert');
+
+//
+//
+var node2html = require('../lib/node2html');
+
+//
+// Mocha
+var describe = global.describe;
+var it = global.it;
+
+
+//
+// Main code
+//
+
+//
+// Tests
+describe('test', function test()
+{
+	it('test', function test()
+	{
+		assert.equal(true, true);
+	});
+});
